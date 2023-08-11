1fadb85724c30afd86b9e656eaebb00a4d9c47f6
Enthony Stevie <enthonyoliveira96@gmail.com>
2023-08-10 19:45:29 -0700

    Implementei uma solução alternativa para ser testada futuramente

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
+// import ProtectedRoute from './ProtectedRoute';
 
 // Pages
 import Login from '../pages/Login';
@@ -9,8 +10,17 @@ import Page404 from '../pages/Page404';
 const Rotas = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<Page404 />} />
+
+      {/* Este é um exemplo para rotas protegidas e que devem ser acessadas de o usuário estiver logado*/}
+      {/*
+        <Route path="/login">
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        </Route>
+      */}
     </Routes>
   );
 };
